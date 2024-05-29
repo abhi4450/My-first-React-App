291ba1b82ed91fc42e11980a738a19a403505f17
abhi4450 <abhishek.anshu1991>
2024-05-29 12:48:39 +0530

    Added First Custom Component named ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <>
       <h1>Let's get started !</h1>
-      <p>I am learning react</p>
+      <ExpenseItem />
     </>
   );
 }
