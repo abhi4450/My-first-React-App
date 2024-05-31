3107566aaa9b27ed94bef2dc0b4b283500f812c4
abhi4450 <abhishek.anshu1991>
2024-05-31 23:45:54 +0530

    gave calendar look to date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
-
 import "./ExpenseItem.css";
 
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 function ExpenseItem(props) {
+  const Month = monthNames[props.date.getMonth()];
+  const Year = props.date.getFullYear();
+  const Date = props.date.getDate();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{Month}</div>
+        <div>{Year}</div>
+        <div>{Date}</div>
+      </div>
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
