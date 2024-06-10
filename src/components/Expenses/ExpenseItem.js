d8a337f05d096c032111786b4a8bd58891c267e8
abhi4450 <abhishek.anshu1991>
2024-06-10 11:56:01 +0530

    used useState hook to change the title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,12 @@
+import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
   const buttonClickHandler = (event) => {
-    console.log("Button clicked");
+    setTitle("New Title");
   };
 
   return (
@@ -12,7 +14,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
         <button onClick={buttonClickHandler}>Change Title</button>
       </div>
