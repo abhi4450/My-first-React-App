04a4be92ec7abe6735e86fcfaa3444878d04a01f
abhi4450 <abhishek.anshu1991>
2024-06-10 11:32:09 +0530

    added event listener to button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,23 @@
-
 import ExpenseDate from "./ExpenseDate";
-import Card  from "../UI/Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-
+  const buttonClickHandler = (event) => {
+    console.log("Button clicked");
+  };
 
   return (
     <Card className="expense-item">
-   
-     <ExpenseDate date={props.date} />
-   
-     
+      <ExpenseDate date={props.date} />
+
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={buttonClickHandler}>Change Title</button>
       </div>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
