cf3f1df61d53e822bad44a45eac1e2dfcc768769
abhi4450 <abhishek.anshu1991>
2024-06-13 14:33:06 +0530

    added functionality to filter expenses based on selected year

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const expensesList = [
   { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
-  { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
-  { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+  { id: "2", date: new Date(2024, 3, 25), title: "Book", price: 10 },
+  { id: "3", date: new Date(2025, 10, 11), title: "Pen", price: 1 },
   { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
 ];
 
 const App = () => {
-  const [expenses, setExpenses] = useState(expensesList)
- 
+  const [expenses, setExpenses] = useState(expensesList);
 
   const addExpenseDataHandler = (expensesData) => {
-
-      
-         setExpenses((prevState) => {
-           const expenseData = [...prevState, expensesData]
-           return expenseData;
-         })
-    };
-  
+    setExpenses((prevState) => {
+      const expenseData = [...prevState, expensesData];
+      return expenseData;
+    });
+  };
 
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,36 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.expenses.filter((exp) => {
+    return exp.date.getFullYear().toString() === filteredYear;
+  });
   return (
     <Card className="expenses">
-      {props.expenses.map((expense, index) => {
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {filteredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
-            date={expense.date}
             title={expense.title}
+            date={expense.date}
             price={expense.price}
           />
         );
       })}
     </Card>
   );
-}
+};
+
 export default Expenses;
